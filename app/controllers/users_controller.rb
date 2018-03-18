385727afe77db885fb6339361bd6a33df811ab71
Rita <rita.lu@yahoo.com.tw>
2018-03-18 23:23:09 +0800

    revise is_admin to is_admin? & moving is_admin? method from controller to model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,4 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  #只有 id 是 1 的 User 才是 admin
-  def is_admin
-    self.id == 1
-  end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  #只有 id 是 1 的 User 才是 admin
+  def is_admin?
+    self.id == 1
+  end
 end
