eafb0b42c1126b166bb21035c5da72ee3619d1ca
Rita <rita.lu@yahoo.com.tw>
2018-03-18 19:50:37 +0800

    add is_admin in index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
 
+   def index
+    @users = User.all
+  end
+
   #只有 id 是 1 的 User 才是 admin
-  def is_admin?
+  def is_admin
     self.id == 1
   end
-  
+
 end
